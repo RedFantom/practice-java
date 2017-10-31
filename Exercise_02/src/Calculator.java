66c2ee43436cdddd96b211ec54e3cdd0e0b8103e
RedFantom <redfantom@outlook.com>
2017-10-31 19:54:11 +0100

    Add Exercise 02

---
+++ Exercise_02/src/Calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        float param;
+        char operation;
+        float total = 0;
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Command (+/-/*/:/q): ");
+            operation = input.next().charAt(0);
+            if (operation == 'q') break;
+            else if (operation != '+' && operation != '-' && operation != '*' && operation != ':' && operation != '/') {
+                System.out.println("Unknown command: " + operation);
+            }
+            System.out.println("Value: ");
+            param = input.nextFloat();
+            if (operation == '+') {
+                total += param;
+            } else if (operation == '-') {
+                total -= param;
+            } else if (operation == '*') {
+                total *= param;
+            } else if (operation == ':' || operation == '/') {
+                total /= param;
+            }
+            System.out.println("Total: " + total);
+        }
+        System.exit(0);
+    }
+}
