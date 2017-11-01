d90da401985ec90e9c475167baf0d1fddd6cef57
RedFantom <redfantom@outlook.com>
2017-11-01 10:28:12 +0100

    Add Exercise 02 file header

--- Exercise_02/src/Calculator.java
+++ Exercise_02/src/Calculator.java
@@ -1,3 +1,10 @@
+/*
+Exercise 2 : Calculator
+
+Simple Java program to practice input, output and if/else statements
+
+Copyright (C) RedFantom 2017
+*/
 import java.util.Scanner;
 
 public class Calculator {
